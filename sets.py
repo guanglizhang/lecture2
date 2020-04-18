460f35bf8e6f29c298931f0af341894877be9fc5
Guangli Zhang <guanglizhang@ucsb.edu>
2020-04-18 15:49:52 -0700

    add more comments

--- sets.py
+++ sets.py
@@ -1,5 +1,6 @@
 s = set()
-# set requires input to be unique
+# Sets are unordered collection of unique items. Because they are unordered, they cannot be indexed.
+# s is a set, an unordered collection of unique items
 s.add(1)
 s.add(3)
 s.add(5)
