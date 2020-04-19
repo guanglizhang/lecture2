74d5a5b7db40b8f5e97dc3f9049cb7be27694859
Guangli Zhang <guanglizhang@ucsb.edu>
2020-04-19 13:24:02 -0700

    allow submit by get and post

--- forms/application.py
+++ forms/application.py
@@ -6,9 +6,12 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/hello", methods=["POST"])
+@app.route("/hello", methods=["GET","POST"]) #allow both get (directly input link) and post methods
 def hello():
-    name = request.form.get("name") # take the request the user made, access the form,
-                                    # and store the field called `name` in a Python variable also called `name`
-    return render_template("hello.html", name=name)
+    if request.method == "GET": #if use get method, will be directed to the page "please submit the form instead"
+        return "Please submit the form instead."
+    else:
+        name = request.form.get("name") # take the request the user made, access the form,
+                                        # and store the field called `name` in a Python variable also called `name`
+        return render_template("hello.html", name=name)
 
