44a1056eb6535ee5bfb5c3bf2b543b2f2435a140
Guangli Zhang <guanglizhang@ucsb.edu>
2020-04-19 13:36:26 -0700

    add 'app=Flask(__name__)'

--- notes/application.py
+++ notes/application.py
@@ -6,6 +6,8 @@ from flask import Flask, render_template, request, session # gives access to a v
 from flask_session import Session # an additional extension to sessions which allows them
                                   # to be stored server-side
 
+app = Flask(__name__)
+
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
